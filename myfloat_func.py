9831d8866901296fe3d12b8c19f53067578f1a21
jscachayam <30726325+jscachayam@users.noreply.github.com>
2018-11-28 17:37:35 -0500

    terminado

--- myfloat_func.py
+++ myfloat_func.py
@@ -1,6 +1,4 @@
 def imprimir(a):
-    if type(a)=!tuple:
-        return a
     a[0].reverse()
     str1 = ''.join(str(e) for e in a[0])
     str2 = ''.join(str(e) for e in a[1])
@@ -8,8 +6,40 @@ def imprimir(a):
     str3=str1+","+str2
     return str3
 
+def entero(n):
+    if type(n)==int:
+        if n>=0:
+            e=list('+' + str(n))
+        else:
+            e=list(str(n))
+        E=[0]*len(e)
+        for i in range(len(e)):
+            if i==0:
+                E[0]=e[0]
+            else:
+                E[i]=int(e[i])
+        D=[0]
+    
+    elif type(n)==float:
+        if n>=0:
+            ll=len(str(int(n)))+1
+            num=list('+' + str(n))
+        else:
+            ll=len(str(int(n)))
+            num=list(str(n))
+        E=[0]*ll
+        D=[0]*(len(num)-ll-1)
+        for i in range(ll):
+            if i==0:
+                E[0]=num[0]
+            else:
+                E[i]=int(num[i])
+        for i in range(ll+1,len(num)):
+            D[i-ll-1]=int(num[i])
+    E.reverse()  
+    return (E,D)
+
 def suma(a,b):
- 
     E1=a[0][::-1]
     D1=a[1][::]
     E2=b[0][::-1]
@@ -17,23 +47,22 @@ def suma(a,b):
     num1=(E1,D1)
     num2=(E2,D2)
 
-    if len(E1)!=len(E2):
+    if len(E1)>len(E2):
         ss=len(E1)-len(E2)    
-        if ss>0:
-            for i in range(ss):
-                num2[0].insert(1,0)
-        else:
-            for i in range(ss):
-                num1[0].insert(1,0) 
-   
-    if len(D1)!=len(D2):
+        for i in range(ss):
+            num2[0].insert(1,0)    
+    elif len(E1)<len(E2):
+        ss=len(E2)-len(E1)    
+        for i in range(ss):
+            num1[0].insert(1,0)    
+    if len(D1)>len(D2):
         ss=len(D1)-len(D2)    
-        if ss>0:
-            for i in range(ss):
-                num2[1].insert(len(D2),0)
-        else:
-            for i in range(ss):
-                num1[1].insert(len(D1),0)    
+        for i in range(ss):
+            num2[1].insert(len(D2),0)    
+    elif len(D1)<len(D2):
+        ss=len(D2)-len(D1)    
+        for i in range(ss):
+            num1[1].insert(len(D1),0)    
    
     E3=[0]*len(E1)
     D3=[0]*len(D1)
@@ -64,235 +93,103 @@ def suma(a,b):
             num3[0][0]='+'
         else:
             num3[0][0]='-'
+        
     else:
-        m=0
-        n=0
-        for i in range(len(E3)-1,-1,-1):
-            if i==0:
+        if num1[0][0]=='+':
+            sign1=1
+            sign1Tmp=1
+        else:
+            sign1=-1
+            sign1Tmp=-1
+
+        if num2[0][0]=='+':
+            sign2=1
+            sign2Tmp=1
+        else:
+            sign2=-1
+            sign2Tmp=-1
+
+        numM=0
+
+        for i in range(1,len(E3),1):
+            if num1[0][i]>num2[0][i]:
+                numM=1
                 break
-            elif num1[0][i]<num2[0][i]:
-                mayore=num2[0]
-                menore=num1[0]
-                num3[0][0]=num2[0][0]
+            elif num2[0][i]>num1[0][i]:
+                numM=2
                 break
-            elif num1[0][i]>num2[0][i]:
-                mayore=num1[0]
-                menore=num2[0]
-                num3[0][0]=num1[0][0]
-                break
-            elif num1[0][i]==num2[0][i]:
-                m=m+1
-                continue
+        
+        if numM==0:
+            for i in range(len(D3)):
+                if num1[1][i]>num2[1][i]:
+                    numM=1
+                    break
+                elif num2[1][i]>num1[1][i]:
+                    numM=2
+                    break
             
+        if numM==1 and sign1==-1:
+            sign1Tmp=1
+            sign2Tmp=-1
+        if numM==2 and sign2==-1:
+            sign1Tmp=-1
+            sign2Tmp=1
+                
         for i in range(len(D3)-1,-1,-1):
-            if num1[1][i]<num2[1][i]:
-                mayord=num2[1]
-                menord=num1[1]
-                if num3[0][0]==0:
-                    num3[0][0]=num1[0][0]
-                break
-            elif num1[1][i]>num2[1][i]:
-                mayord=num1[1]
-                menord=num2[1]
-                if num3[0][0]==0:
-                    num3[0][0]=num2[0][0]
-                break
-            elif num1[1][i]==num2[1][i]:
-                n=n+1
-                continue
-        if m==len(E3)-1:
-            mayore=num1[0]
-            menore=num2[0]
-        if n==len(D3):
-            mayord=num1[1]
-            menord=num2[1]
-        if m==len(E3)-1 and n==len(D3):
-            num3[0][0]='+'
-            
-        mayor=(mayore,mayord)
-        menor=(menore,menord)
-        for i in range(len(D3)-1,-1,-1):
-             num3[1][i]=num3[1][i] + menor[1][i] - mayor[1][i] 
-             if num3[1][i]>9:
-                 num3[1][i]=num3[1][i]-10
-                 if i==0:
-                     num3[0][-1]=1
-                 else:
-                    num3[1][i-1]=1
-             if num3[1][i]<0:
+            num3[1][i]=num3[1][i] + sign1Tmp*num1[1][i] + sign2Tmp*num2[1][i]
+            if num3[1][i]<0:
                 num3[1][i]=num3[1][i]+10
                 if i==0:
                     num3[0][-1]=-1
                 else:
                     num3[1][i-1]=-1
+
         for i in range(len(E3)-1,-1,-1):
             if i==0:
                 break
-            num3[0][i]=num3[0][i] + mayor[0][i] - menor[0][i]
-            if num3[0][i]>9:
-                if i==1:
-                    num3[0][i]=num3[0][i]-10
-                    num3[0].insert(1,1)
-                else:
-                    num3[0][i]=num3[0][i]-10
-                    num3[0][i-1]=1
+            num3[0][i]=num3[0][i] + sign1Tmp*num1[0][i] + sign2Tmp*num2[0][i]
             if num3[0][i]<0:
-                num3[0][i]=num3[0][i]+10
-                num3[0][i-1]=-1
-    
+                if i==1:
+                    num3[0][i]=num3[0][i]+10
+                    num3[0][0]=-1
+                else:
+                    num3[0][i]=num3[0][i]+10
+                    num3[0][i-1]=-1
+
+        if numM==1:
+            if sign1>0:
+                num3[0][0]='+'
+            else:
+                num3[0][0]='-'
+        elif numM==2:
+            if sign2>0:
+                num3[0][0]='+'
+            else:
+                num3[0][0]='-'
+        else:
+            num3[0][0]='+'
+        
     while num3[0][1]==0 and len(num3[0])>2:
         num3[0].pop(1)
     while num3[1][-1]==0 and len(num3[1])>1 :
         num3[1].pop(-1)
-
     num3[0].reverse()
     return num3
 
 def resta(a,b):
- 
-    E1=a[0][::-1]
-    D1=a[1][::]
     E2=b[0][::-1]
     D2=b[1][::]
-    num1=(E1,D1)
+
+    if E2[0]=='+':
+        E2[0]='-'
+    else:
+        E2[0]='+'
+    E2.reverse()
     num2=(E2,D2)
+    k=suma(a,num2)
     
-    if num2[0][0]=='+':
-        num2[0][0]='-'
-    else:
-        num2[0][0]='+'
-        
-    if len(E1)!=len(E2):
-        ss=len(E1)-len(E2)    
-        if ss>0:
-            for i in range(ss):
-                num2[0].insert(1,0)
-        else:
-            for i in range(ss):
-                num1[0].insert(1,0) 
-   
-    if len(D1)!=len(D2):
-        ss=len(D1)-len(D2)    
-        if ss>0:
-            for i in range(ss):
-                num2[1].insert(len(D2),0)
-        else:
-            for i in range(ss):
-                num1[1].insert(len(D1),0)    
-   
-    E3=[0]*len(E1)
-    D3=[0]*len(D1)
-    num3=(E3,D3)
- 
-    if num1[0][0]==num2[0][0]:
-        for i in range(len(D3)-1,-1,-1):
-            num3[1][i]=num3[1][i] + num1[1][i] + num2[1][i]
-            if num3[1][i]>9:
-                num3[1][i]=num3[1][i]-10
-                if i==0:
-                    num3[0][-1]=1
-                else:
-                    num3[1][i-1]=1
+    return k
 
-        for i in range(len(E3)-1,-1,-1):
-            if i==0:
-                break
-            num3[0][i]=num3[0][i] + num1[0][i] + num2[0][i]
-            if num3[0][i]>9:
-                if i==1:
-                    num3[0][i]=num3[0][i]-10
-                    num3[0].insert(1,1)
-                else:
-                    num3[0][i]=num3[0][i]-10
-                    num3[0][i-1]=1   
-        if num1[0][0]=='+':
-            num3[0][0]='+'
-        else:
-            num3[0][0]='-'
-    else:
-        m=0
-        n=0
-        for i in range(len(E3)-1,-1,-1):
-            if i==0:
-                break
-            elif num1[0][i]<num2[0][i]:
-                mayore=num2[0]
-                menore=num1[0]
-                num3[0][0]=num2[0][0]
-                break
-            elif num1[0][i]>num2[0][i]:
-                mayore=num1[0]
-                menore=num2[0]
-                num3[0][0]=num1[0][0]
-                break
-            elif num1[0][i]==num2[0][i]:
-                m=m+1
-                continue
-            
-        for i in range(len(D3)-1,-1,-1):
-            if num1[1][i]<num2[1][i]:
-                mayord=num2[1]
-                menord=num1[1]
-                if num3[0][0]==0:
-                    num3[0][0]=num1[0][0]
-                break
-            elif num1[1][i]>num2[1][i]:
-                mayord=num1[1]
-                menord=num2[1]
-                if num3[0][0]==0:
-                    num3[0][0]=num2[0][0]
-                break
-            elif num1[1][i]==num2[1][i]:
-                n=n+1
-                continue
-        if m==len(E3)-1:
-            mayore=num1[0]
-            menore=num2[0]
-        if n==len(D3):
-            mayord=num1[1]
-            menord=num2[1]
-        if m==len(E3)-1 and n==len(D3):
-            num3[0][0]='+'
-            
-        mayor=(mayore,mayord)
-        menor=(menore,menord)
-        for i in range(len(D3)-1,-1,-1):
-             num3[1][i]=num3[1][i] + menor[1][i] - mayor[1][i] 
-             if num3[1][i]>9:
-                 num3[1][i]=num3[1][i]-10
-                 if i==0:
-                     num3[0][-1]=1
-                 else:
-                    num3[1][i-1]=1
-             if num3[1][i]<0:
-                num3[1][i]=num3[1][i]+10
-                if i==0:
-                    num3[0][-1]=-1
-                else:
-                    num3[1][i-1]=-1
-        for i in range(len(E3)-1,-1,-1):
-            if i==0:
-                break
-            num3[0][i]=num3[0][i] + mayor[0][i] - menor[0][i]
-            if num3[0][i]>9:
-                if i==1:
-                    num3[0][i]=num3[0][i]-10
-                    num3[0].insert(1,1)
-                else:
-                    num3[0][i]=num3[0][i]-10
-                    num3[0][i-1]=1
-            if num3[0][i]<0:
-                num3[0][i]=num3[0][i]+10
-                num3[0][i-1]=-1
-    
-    while num3[0][1]==0 and len(num3[0])>2:
-        num3[0].pop(1)
-    while num3[1][-1]==0 and len(num3[1])>1 :
-        num3[1].pop(-1)
-
-    num3[0].reverse()
-    return num3
-    
 def multiplicacion(a,b):
     
     E1=a[0][::-1]
@@ -432,13 +329,20 @@ def comparacion(a,b):
     if resta(a,b)==([0,'+'],[0]):
         print("las tuplas son iguales")
     else:
-        print("las tuplas son diferentes")      
+        print("las tuplas son diferentes")         
 
-def pi(n):
-    pi=0
-    for k in range(n+1):
-        pi += 2 * (-1)**k * 3**(1/2 - k) / (2 * k + 1)
-    return pi
-
+def pi():
+    x=entero(0) 
+    for k in range(500000):
+        if k%2!=0:
+            numerador=entero(-4)
+        else:
+            numerador=entero(4)
+        denominador=suma(multiplicacion(entero(2),entero(k)),entero(1))
+        div=division(numerador,denominador)    
+        x=suma(x,div)
+        
+    return x
 
 if __name__ == "__main__":
+    print(imprimir(pi()))
